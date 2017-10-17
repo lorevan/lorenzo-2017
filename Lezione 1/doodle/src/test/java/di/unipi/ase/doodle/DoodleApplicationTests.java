4a47c372d4e825e7f1602ddb138e55474363dba9
Lorenzo <lorenzovangi@gmail.com>
2017-10-17 23:22:34 +0200

    New Commit

---
+++ Lezione 1/doodle/src/test/java/di/unipi/ase/doodle/DoodleApplicationTests.java
@@ -0,0 +1,16 @@
+package di.unipi.ase.doodle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoodleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
